47d5ec7a5b1566c37b68f17cc7d11d4668d046e4
es404020 <es404020@gmail.com>
2023-07-12 11:05:54 +0100

    ok

--- image/settings.py
+++ image/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "playground",
-    # "debug_toolbar",
+    "debug_toolbar",
     'compressor', # new
     "django_browser_reload"
 ]
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    # "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
